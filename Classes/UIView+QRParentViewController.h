1e0ae72b7a7934af3ac8e61bdb08575d5d6b9886
Taro Kobayashi <9re.3000@gmail.com>
2014-02-02 15:16:35 +0900

    snippet to get reference of parent view controller

---
+++ Classes/UIView+QRParentViewController.h
@@ -0,0 +1,14 @@
+//
+//  UIView+QRParentViewController.h
+//  Pods
+//
+//  Created by Taro Kobayashi on 02/02/2014.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface UIView (QRParentViewController)
+- (UIViewController *)parentViewController;
+@end
---
+++ Classes/UIView+QRParentViewController.m
@@ -0,0 +1,28 @@
+//
+//  UIView+QRParentViewController.m
+//  Pods
+//
+//  Created by Taro Kobayashi on 02/02/2014.
+//
+//
+
+#import "UIView+QRParentViewController.h"
+
+@implementation UIView (QRParentViewController)
+
+- (UIViewController *)parentViewController
+{
+    UIView *view = self;
+    while (view) {
+        UIResponder *responder = [view nextResponder];
+        if ([responder isKindOfClass:[UIViewController class]])
+        {
+            return (UIViewController *)responder;
+        }
+        view = self.superview;
+    }
+    
+    return nil;
+}
+
+@end
